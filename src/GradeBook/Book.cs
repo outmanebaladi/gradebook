0a504d8e4e87e3a95bbd984f2a4fc6b9b3900784
outmanebaladi <outmanebaladi22@gmail.com>
2019-12-24 14:30:29 -0800

    initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{   public interface IBook{
+        public Name {get;}
+        void AddGrade(double grade);
+        Statistics GetStatistics();
+        event GradeAddedDelegate GradeAdded;
+    }
+    public abstract class Book : IBook
+    {
+        List<double> grades;
+        public string Name;
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+        public void AddGrade(double grade){
+            grades.Add(grade);
+        }
+
+        public Statistics GetStatistics(){
+
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.Low = double.MaxValue;
+            result.High = double.MinValue;
+
+            foreach(var grade in grades){
+                result.Average += grade;
+                result.Low = Math.Min(result.Low, grade);
+                result.High = Math.Max(result.High, grade);
+            }
+            result.Average /= grades.Count;
+
+            return result;
+
+        }
+    } 
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Outmane Baladi book");
+            book.AddGrade(52.6);
+            book.AddGrade(20.2);
+            book.AddGrade(15.3);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The average grade is: {stats.Average:N1}");
+            Console.WriteLine($"The lowest grade is: {stats.Low}");
+            Console.WriteLine($"The highest grade is: {stats.High}");
+        }
+    }
+}
