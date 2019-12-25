953209875d255076072bba6ee4b6c542a0c225b1
Outmane Baladi <outmanebaladi22@gmail.com>
2019-12-25 17:44:44 +0100

    OOP: encapsulation, inheritence and polymorphisme

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,16 +7,29 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Outmane Baladi book");
-            book.GradeAdded += OnGradeAdded;
+            IBook book = new DiskBook("Outmane Baladi");
             book.GradeAdded += OnGradeAdded;
 
-            while(true)
+            EnterGrades(book);
+            var stats = book.GetStatistics();
+
+            Console.WriteLine(InMemoryBook.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The average grade is: {stats.Average:N1}");
+            Console.WriteLine($"The lowest grade is: {stats.Low}");
+            Console.WriteLine($"The highest grade is: {stats.High}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit");
                 var input = Console.ReadLine();
 
-                if(input == "q"){
+                if (input == "q")
+                {
                     break;
                 }
 
@@ -25,24 +38,19 @@ namespace GradeBook
                     var grade = double.Parse(input);
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex){
+                catch (ArgumentException ex)
+                {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex){
+                catch (FormatException ex)
+                {
                     Console.WriteLine(ex.Message);
                 }
-                finally{
+                finally
+                {
                     Console.WriteLine("**");
                 }
             }
-            var stats = book.GetStatistics();
-
-            Console.WriteLine(Book.CATEGORY);
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The average grade is: {stats.Average:N1}");
-            Console.WriteLine($"The lowest grade is: {stats.Low}");
-            Console.WriteLine($"The highest grade is: {stats.High}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
 
         static void OnGradeAdded(object sender, EventArgs e){
